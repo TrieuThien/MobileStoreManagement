d0794e52bdfe67ea04bd3cbdf1b810d0d57aef89
Huỳnh Phú Quí <hphuqui33@gmail.com>
2025-04-26 15:01:19 +0700

    add UI

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace MobileStoreManagement.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap exit1 {
+            get {
+                object obj = ResourceManager.GetObject("exit1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap export {
             get {
                 object obj = ResourceManager.GetObject("export", resourceCulture);
