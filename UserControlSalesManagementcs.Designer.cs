a90bd5522e418d1ac4b5b50a53b1bbec5b7f91c4
Huỳnh Phú Quí <hphuqui33@gmail.com>
2025-04-26 14:39:36 +0700

    add UI

--- UserControlSalesManagementcs.Designer.cs
+++ UserControlSalesManagementcs.Designer.cs
@@ -28,28 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(333, 272);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bán Hàng";
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(654, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BÁN HÀNG";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(109, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 557);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(459, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(881, 557);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Quản lý phiếu đặt hàng";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Quản lý phiếu mua hàng";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 52);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Quản lý phiếu bảo hành";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(71, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 52);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Quản lý phiếu giao hàng";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // UserControlSalesManagementcs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MinimumSize = new System.Drawing.Size(622, 569);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(1427, 662);
             this.Name = "UserControlSalesManagementcs";
-            this.Size = new System.Drawing.Size(622, 569);
+            this.Size = new System.Drawing.Size(1427, 662);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +122,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
